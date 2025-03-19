8f372e520e0d06ac91f4d56bca10eb7e6f75d80a
Sergey Zhul <s.zhul@bftcom.com>
2025-03-19 20:51:46 +0700

    new test

--- src/test/java/demoqa/TestBoxTest.java
+++ src/test/java/demoqa/TestBoxTest.java
@@ -3,7 +3,7 @@ package demoqa;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
@@ -17,19 +17,29 @@ public class TestBoxTest {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.pageLoadTimeout = 100000;
+        Configuration.baseUrl = "https://demoqa.com";
     }
 
     @Test
     public void testBoxTest() {
-        open("https://demoqa.com/text-box");
+        String username = "Ivan";
+        String email = "ivankov@mail.ru";
+        String currentAddress = "Egypt";
+        String permanentAddress = "Russia";
+
+        open("/text-box");
         $(".text-center").shouldHave(text("Text Box"));
-        $("#userName").setValue("Ivan");
-        $("#userEmail").setValue("ivankov@mail.ru");
-        $("#currentAddress").setValue("Egypt");
-        $("#permanentAddress").setValue("Russia");
+        $("#userName").setValue(username);
+        $("#userEmail").setValue(email);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
         $("#submit").click();
         $("#output").shouldBe(visible);
 
         $$(".border.col-md-12.col-sm-12 p").shouldHave(size(4));
+        $("#output #name").shouldHave(text(username));
+        $("#output #email").shouldHave(text(email));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
     }
 }
