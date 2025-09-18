4c3c3b1f9b19d88e2bf1b2f527966e5db3aae8d2
Sergey Zhul <s.zhul@bftcom.com>
2025-09-18 18:02:18 +0700

    Вынесены методы remote в TestBaseExtended

--- src/test/java/tests/demoqa/TestBoxWithRemoteDriverTest.java
+++ src/test/java/tests/demoqa/TestBoxWithRemoteDriverTest.java
@@ -1,16 +1,8 @@
 package tests.demoqa;
 
-
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.util.HashMap;
+import tests.TestBaseExtended;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
@@ -19,32 +11,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 
-public class TestBoxWithRemoteDriverTest {
-
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.pageLoadTimeout = 100000;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("browserName", "chrome");
-        capabilities.setCapability("browserVersion", "128.0");
-        capabilities.setCapability("selenoid:options", new HashMap<String, Object>() {{
-            put("enableVNC", true);
-            put("enableVideo", true);
-        }});
-        Configuration.browserCapabilities = capabilities;
-    }
-
-    @BeforeEach
-    void addListener() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-    }
+public class TestBoxWithRemoteDriverTest extends TestBaseExtended {
 
     @Test
     @Tag("remote")
@@ -68,6 +35,5 @@ public class TestBoxWithRemoteDriverTest {
         $("#output #email").shouldHave(text(email));
         $("#output #currentAddress").shouldHave(text(currentAddress));
         step("Проверить адрес", () -> $("#output #permanentAddress").shouldHave(text(permanentAddress)));
-//        $("#output #permanentAddress").shouldHave(text(permanentAddress));
     }
 }
