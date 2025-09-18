650ae23c585b99c81af5c0b7257ebb91da5c7ec2
Sergey Zhul <s.zhul@bftcom.com>
2025-09-18 15:25:10 +0700

    add new graddle + some tests

---
+++ src/test/java/tests/simple/PositiveTests.java
@@ -0,0 +1,30 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("simple")
+public class PositiveTests {
+
+    @Test
+    public void positiveTest1() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void positiveTest2() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void positiveTest3() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void positiveTest4() {
+        assertTrue(true);
+    }
+}
