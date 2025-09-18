516bbec45570e0cf8c31eb2e276c031d5f862616
Sergey Zhul <s.zhul@bftcom.com>
2025-09-18 17:33:46 +0700

    add remote test

---
+++ src/test/java/tests/demoqa/TestBoxWithRemoteDriverTest.java
@@ -0,0 +1,67 @@
+package tests.demoqa;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class TestBoxWithRemoteDriverTest {
+
+    @BeforeAll
+    static void beforeAll() throws MalformedURLException {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.pageLoadTimeout = 100000;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "128.0");
+        capabilities.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            put("enableVNC", true);
+            put("enableVideo", true);
+        }});
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @Test
+    @Tag("remote")
+    public void testBoxTest() {
+        String username = "Ivan";
+        String email = "ivankov@mail.ru";
+        String currentAddress = "Egypt";
+        String permanentAddress = "Russia";
+
+        open("/text-box");
+        $(".text-center").shouldHave(text("Text Box"));
+        $("#userName").setValue(username);
+        $("#userEmail").setValue(email);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+        $("#output").shouldBe(visible);
+
+        $$(".border.col-md-12.col-sm-12 p").shouldHave(size(4));
+        $("#output #name").shouldHave(text(username));
+        $("#output #email").shouldHave(text(email));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+    }
+}
