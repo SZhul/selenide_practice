4511646eae951507e82ef84696ffb448330b9789
Sergey Zhul <s.zhul@bftcom.com>
2025-09-25 09:53:07 +0700

    added test tasks

---
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,38 @@
+package tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class SystemPropertiesTests {
+
+    @Test
+    void simplePropertyTest() {
+        String browserName = System.getProperty("browserName");
+        System.out.println(browserName); //null
+    }
+
+    @Test
+    void simplePropertyTest1() {
+        System.setProperty("browserName", "opera");
+        String browserName = System.getProperty("browserName");
+        System.out.println(browserName); //opera
+    }
+
+    @Test
+    void simplePropertyTest2() {
+        String browserName = System.getProperty("browserName", "firefox");
+        System.out.println(browserName); //opera
+    }
+
+    @Test
+    @Tag("oneProperty")
+    void simplePropertyTest3() {
+        String browserName = System.getProperty("browserName", "firefox");
+        System.out.println(browserName);
+        // gradle clean one_property_test
+        // gradle clean one_property_test -DbrowserName=firefox
+        // gradle clean one_property_test -DbrowserName="opera_headless" если два слова, берем в кавычках
+    }
+}
